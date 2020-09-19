b62eeb8fa76876184e99961a4d8fc3d59bcb80ba
Kadota Masakazu <ea6in52ieg45@gmail.com>
2020-09-19 20:46:05 +0900

    test_comit

---
+++ groupwork/src/group_even/Group_Even_Main.java
@@ -0,0 +1,10 @@
+package group_even;
+
+public class Group_Even_Main {
+
+	public static void main(String[] args) {
+		AcceptUserInput startApp = new AcceptUserInput();
+		startApp.inputReception();
+	}
+
+}
